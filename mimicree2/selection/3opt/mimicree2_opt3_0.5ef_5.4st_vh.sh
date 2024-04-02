d7faaf97bf447593ac654ebc41d1492ca71d530b
cloudweather34 <45484376+cloudweather34@users.noreply.github.com>
2024-04-02 12:00:54 -0400

    Add files via upload

---
+++ mimicree2/selection/3opt/mimicree2_opt3_0.5ef_5.4st_vh.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##evolution of phenotypic variance 
+##MimicrEE2
+##WYLai 20191101
+##WYLai 20210805 updated $5 h^2
+
+echo “name”
+echo $1
+echo “number of contributing loci”
+echo $2
+echo “repicate”
+echo $3
+echo “effectsize_distribution”
+echo $4
+
+date
+mkdir $1_$2
+cd $1_$2
+mkdir tmp
+for	((i=1;i<=$3;i=i+1)); do
+	hr=`gshuf -n 1 /Volumes/cluster/Wei-Yun/simulation/heri_trudy_micro.txt`
+	Rscript ../pick-SNPs-QTL_0.5.R -I ../mimihap_effectsize_0.5_100.rds -L $2 -O ./ -R $i -D $4
+	java -jar /Volumes/cluster/Wei-Yun/simulation/mim2-v208.jar qff --haplotypes-g0 selectedloci_${2}_$i.mimhap --sex ../sexinfo.txt --recombination-rate ../dsim.rr_X_LDjump-LOESS-0.1.txt --population-size ../population-size_300.txt --effect-size effectsize_${4}_${2}_$i.txt --fitness-function ../fitness_function_tmp.txt --heritability $hr --snapshots 1 --replicate-runs 1 --output-sync tmp.gz --output-gpf tmp.gpf --output-dir tmp/ --threads 1
+	Rscript ../generate_fitness_function_3opt_5.4st.R -I tmp.gpf -O ./ -L $2 -R $i
+	mkdir $1_$2_$i
+	java -jar /Volumes/cluster/Wei-Yun/simulation/mim2-v208.jar qff --haplotypes-g0 selectedloci_${2}_$i.mimhap --sex ../sexinfo.txt --recombination-rate ../dsim.rr_X_LDjump-LOESS-0.1.txt --population-size ../population-size_300.txt --effect-size effectsize_${4}_${2}_$i.txt --fitness-function fitnessfunction_$2_$i.txt --heritability $hr --snapshots 1,5,10,15,20,25,35,50,75,100,200 --replicate-runs 1 --output-sync $1_$2_$i.gz --output-gpf $1_$2_$i.gpf --output-dir $1_$2_$i/ --threads 1
+done
+
+date
